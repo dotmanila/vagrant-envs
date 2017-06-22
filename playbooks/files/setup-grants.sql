f1832d02c0627234d77a78acdede02459075aeab
Jervin Real <accounts@dotmanila.com>
2017-06-22 09:23:03 -0400

    PXC+ProxySQL choice between 5.6 and 5.7

--- playbooks/files/setup-grants.sql
+++ playbooks/files/setup-grants.sql
@@ -1,3 +1,4 @@
-GRANT ALL ON *.* TO 'percona'@'192.168.56.%' IDENTIFIED BY 'percona';
-GRANT ALL ON *.* TO 'percona'@'127.0.0.1' IDENTIFIED BY 'percona';
-GRANT ALL ON *.* TO 'percona'@'localhost' IDENTIFIED BY 'percona';
+GRANT ALL ON *.* TO 'percona'@'192.168.56.%' IDENTIFIED BY 'PERCona2017_';
+GRANT ALL ON *.* TO 'percona'@'127.0.0.1' IDENTIFIED BY 'PERCona2017_';
+GRANT ALL ON *.* TO 'percona'@'localhost' IDENTIFIED BY 'PERCona2017_';
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON  *.* TO 'proxysql'@'192.168.56.%' IDENTIFIED BY 'PERCona2017_'
--- playbooks/files/setup-proxysql.sql
+++ playbooks/files/setup-proxysql.sql
@@ -1,6 +1,14 @@
 REPLACE INTO main.mysql_servers (hostname) 
   VALUES ('192.168.56.82'), ('192.168.56.83'), ('192.168.56.84');
 REPLACE INTO main.mysql_users (username, password)
-  VALUES ('percona', 'percona');
+  VALUES ('percona', 'PERCona2017_');
 LOAD MYSQL SERVERS TO RUNTIME;
 LOAD MYSQL USERS TO RUNTIME;
+
+SAVE MYSQL SERVERS TO DISK;
+SAVE MYSQL USERS TO DISK;
+
+UPDATE main.global_variables SET variable_value='proxysql'
+  WHERE variable_name=='mysql-monitor_username';
+UPDATE main.global_variables SET variable_value='PERCona2017_'
+  WHERE variable_name=='mysql-monitor_password';
